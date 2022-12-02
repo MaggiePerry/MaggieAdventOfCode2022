b1d04fd47945ec08ade96785ee867a8484a884ec
MaggiePerry <maggie.c.perry@gmail.com>
2022-12-02 18:10:07 -0400

    Day-1: Create function to add array sum. Solved second puzzle

--- day-1/gribs.py
+++ day-1/gribs.py
@@ -1,13 +1,34 @@
-import numpy as np
+# Function to add total sum of array values
+def sum(foods) :
+    total = 0
 
+    for food in foods :
+        total += int(food)
+    return total
+
+# Open inputs.txt
 with open('day-1/input.txt') as whosThis:
+    # Make array of elfs
     weAreElves = whosThis.read().split('\n\n');
-    pleaseINeedFood = []
+    iHaveTheFood = []
+    # Loop through each elf
     for elf in weAreElves :
+        # Split the values for each elf
         elf = elf.split('\n')
-        iHaveTheFood = 0
-        for foods in elf :
-            iHaveTheFood += int(foods)
-            pleaseINeedFood.append(iHaveTheFood)
-    iHaveTheMostFood = np.max(pleaseINeedFood)
-    print(iHaveTheMostFood)
+        iHaveTheFood.append(sum(elf))
+
+    # Get the highest value
+    print('Most snacks:', max(iHaveTheFood))
+
+    # Sort the array of calorie values
+    weSortedAllTheFoods = sorted(iHaveTheFood)
+
+    # Get top 3 values
+    theBestThreeElves = weSortedAllTheFoods[-3 : ]
+
+    print("Top {} values for snacks:".format(3), theBestThreeElves)
+
+    # Get sum of top 3 values
+    woopWeHaveMoreFood = sum(theBestThreeElves)
+
+    print(woopWeHaveMoreFood)
